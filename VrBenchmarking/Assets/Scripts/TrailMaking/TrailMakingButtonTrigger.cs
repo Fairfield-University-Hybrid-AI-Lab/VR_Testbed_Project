a68d7bff67bbd71bb8ff972c12dd4495ea3bfd0a
PrathnaPel <prathnapel@gmail.com>
2021-04-13 18:56:43 -0400

    trail making implementation

---
+++ VrBenchmarking/Assets/Scripts/TrailMaking/TrailMakingButtonTrigger.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UXF;
+
+public class TrailMakingButtonTrigger : MonoBehaviour
+{
+    public Session session;
+    public Material green;
+    public Material red;
+    public Material normal;
+    //Data gameobject
+    public GameObject Grid;
+    public TrailMakingData tmd;
+
+    //change button bg color back to white
+    /*
+    IEnumerator RedColorDuration()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Grid.gameObject.GetComponent<Image>().material = normal;
+    }
+    */
+    private YieldInstruction fadeInstruction = new YieldInstruction();
+    public float fadeTime;
+
+    IEnumerator FadeOut(Image image)
+    {
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+        while (elapsedTime < fadeTime)
+        {
+            yield return fadeInstruction;
+            elapsedTime += Time.deltaTime;
+            c.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
+            image.color = c;
+        }
+        image.color = normal.color;
+    }
+    IEnumerator RestTimeBetweenEachTrial()
+    {
+        yield return new WaitForSeconds(2.0f);
+        session.BeginNextTrialSafe();
+    }
+    public void checkSelected()
+    {
+        string GridName = Grid.GetComponentInChildren<Text>().text;
+
+        Debug.Log(tmd.textPath.Count);
+
+        //check if last value
+        if (tmd.textPath.Count > 0)
+        {
+            Debug.Log(tmd.textPath.First.Value);
+            //correct
+            if (tmd.textPath.First.Value == GridName)
+            {
+                tmd.textPath.RemoveFirst();
+
+                Debug.Log("kim");
+                Grid.GetComponent<Image>().color = green.color;
+                //make button unclickable
+                Grid.GetComponent<Button>().interactable = false;
+            }
+            else //wrong selection
+            {
+                Grid.GetComponent<Image>().color = red.color;
+                tmd.wrong.Add(GridName);
+                //delay
+                StartCoroutine(FadeOut(Grid.GetComponent<Image>()));
+                //then rest the material
+            }
+
+        }
+        if (tmd.textPath.Count == 0)
+        {
+            //record data, correct path is already recorded
+            string temp = string.Join(",", tmd.wrong.ToArray());
+            session.CurrentTrial.result["outcome"] = "success";
+            session.CurrentTrial.result["wrong_path"] = temp;
+
+            //reset data
+            tmd.resetAllData();
+
+            //end trial
+            session.EndCurrentTrial();
+
+            //begin the next trial
+            StartCoroutine(RestTimeBetweenEachTrial());
+
+        }
+
+
+    }
+
+}
---
+++ VrBenchmarking/Assets/Scripts/TrailMaking/TrailMakingTooSlowToCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UXF;
+
+public class TrailMakingTooSlowToCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip nosound;
+    public Session session;
+    public TrailMakingData tmd;
+
+    public void BeginCountdown()
+    {
+        StartCoroutine(Countdown());
+    }
+    public void StopCountdown()
+    {
+        StopAllCoroutines();
+    }
+
+    public IEnumerator ObserveDisappearDuration(float interval_duration)
+    {
+        yield return new WaitForSeconds(interval_duration);
+        tmd.resetAllData();
+        session.BeginNextTrialSafe();
+    }
+    IEnumerator Countdown()
+    {
+        float interval_duration = session.settings.GetFloat("interval_duration");
+        yield return new WaitForSeconds(interval_duration);
+
+        //result
+        session.CurrentTrial.result["outcome"] = "fail";
+        //session.CurrentTrial.result["on path that wrongly selected"] = Array;
+        //session.CurrentTrial.result["correct_path"] = Array;
+
+        //end trail
+        session.EndCurrentTrial();
+        //play fail sound
+        AudioSource.PlayClipAtPoint(nosound, transform.position, 1.0f);
+
+        //duration between each trial
+        float interval_duration_between_trials = 2.0f;
+
+        StartCoroutine(ObserveDisappearDuration(interval_duration_between_trials));
+    }
+}
