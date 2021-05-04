4d6c6e43470f0b2c3ac6914f441a215f78817856
Alex Freedman <48782893+axllency@users.noreply.github.com>
2021-05-04 18:07:56 -0400

    VisualSearch task
    Visual search task implemented in scene titled "FreshSearch"

---
+++ VrBenchmarking/Assets/Scripts/Visual-Search/VisualSearchTimeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UXF;
+
+public class VisualSearchTimeController : MonoBehaviour
+{
+
+    public UXF.Session session;
+    
+    public void BeginCountdown()
+    {
+        StartCoroutine(Countdown());
+    }
+    public void StopCountdown()
+    {
+        StopAllCoroutines();
+    }
+    IEnumerator ObserveDisappearDuration()
+    {
+        float timeForObserveObjectDisappear = session.CurrentTrial.settings.GetFloat("time_between_trials");
+        
+        yield return new WaitForSeconds(timeForObserveObjectDisappear);
+        Debug.Log("Starting next trial from 'time controller'");
+        session.BeginNextTrialSafe();
+    }
+    IEnumerator Countdown()
+    {
+        float timeoutPeriod = session.CurrentTrial.settings.GetFloat("time_limit");
+        Debug.Log("Starting countdown for " + timeoutPeriod + "seconds");
+        yield return new WaitForSeconds(timeoutPeriod);
+
+        Debug.Log("Ending current trial from 'time controller'");
+        session.EndCurrentTrial();
+
+        //AudioSource.PlayClipAtPoint(nosound, transform.position, 1.0f);
+        Debug.Log("Pausing before next trial");
+        StartCoroutine(ObserveDisappearDuration());
+    }
+
+    public void startWaitForNextTrial(){
+        Debug.Log("Pausing before next trial - called externally");
+        StartCoroutine(ObserveDisappearDuration());
+    }
+}
